ca3a2f771ade69abb3cb19a0015dd8a5894bd808
LeonardoFragnan <leonardofragnan@yahoo.com.br>
2023-06-14 23:27:09 -0300

    first commit

---
+++ My Workbench.sql
@@ -0,0 +1,15 @@
+USE sakila;
+-- INSERT INTO language
+-- VALUE
+-- (DEFAULT, 'PORTUGUÊS','2023-06-01 03:19:19'),
+-- (DEFAULT, 'SPANISH','2023-06-01 03:19:19'),
+--  (DEFAULT, 'ITALIAN','2023-06-01 03:19:19');
+
+-- INSERT INTO country 
+-- VALUES 
+	-- (DEFAULT,'BRASIL2','2023-06-01 03:19:19');
+    
+-- INSERT INTO city
+-- VALUES
+	-- (DEFAULT,'São Paulo 2',last_insert_id(),'2023-06-01 03:19:19')
+
